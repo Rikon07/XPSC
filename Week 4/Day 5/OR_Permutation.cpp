42c605bee2f9118141d2780e1215baa7d6757195
Md. Mahedi Hasan (Rikon) <163644418+Rikon07@users.noreply.github.com>
2024-04-16 13:42:27 +0600

    Create OR_Permutation.cpp

---
+++ Week 4/Day 5/OR_Permutation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        for (int i=n; i>0; i--)
+        {
+            cout << i << " ";
+        }
+        cout <<"\n";
+    }
+    return 0;
+}
